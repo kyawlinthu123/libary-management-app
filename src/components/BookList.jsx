9959e3241de2323a9e8001c6ac89835e366c44ff
kyawlinthu123 <kyawlinthu655321@gmail.com>
2024-02-02 10:06:52 +0630

    finished fetching all books

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import IMG from '../assets/img.jpeg'
+import useFetch from '../hooks/useFetch'
 
 export default function BookList() {
+
+  const { data : books , loading , error } = useFetch('http://localhost:3001/books')
+  
+  if (error){
+    return <p>{error}</p>
+  }
+
   return (
-    <div className='grid grid-cols-2 md:grid-cols-4 gap-4 my-3'>
-        {[1,2,3,4,5].map(() => (
-            <div className='p-4 border border-1'>
+    <div>
+      { loading && <p> Loading contents ... </p>}
+      { !!books && (
+        <div className='grid grid-cols-2 md:grid-cols-4 gap-4 my-3'>
+        {books.map((b) => (
+            <div className='p-4 border border-1' key={b.id}>
                 <img src={IMG} alt="book cover" />
                 <div className='text-center space-y-2 mt-3'>
-                    <h1 className='font-bold'> The Three Musketeers </h1>
-                    <p> Lorem ipsum dolor sit </p>
+                    <h1 className='font-bold'> {b.title} </h1>
+                    <p> {b.description} </p>
                     {/* genres  */}
                     <div className='flex flex-wrap'>
-                    {['Novel', 'Historical Novel', 'Adventure'].map( genre => (
-                    <span className='mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500'> {genre} </span>))}
+                    {b.categories.map( category => (
+                    <span key={category} className='mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500'> {category} </span>))}
                     </div>
                 </div>  
             </div>
         ))}
+    </div>
+      )}
     </div>
   )
 }
--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import NavBar from '../../components/NavBar'
+import { SwitchTransition, CSSTransition } from 'react-transition-group'
+import './transition.css'
 
 export default function Layout() {
+
+  // how to find the paths of the website 
+  const location = useLocation();
+  console.log(location.pathname); // console logging
+
   return (
    <div>
     <NavBar/>
-    <div className=' max-w-6xl mx-auto p-3'>
+    <SwitchTransition>
+      <CSSTransition timeout={200} classNames='fade' key={location.pathname}>
+      <div className=' max-w-6xl mx-auto p-3'>
     <Outlet />
     </div>
+      </CSSTransition>
+    </SwitchTransition>
    </div>
   )
 }
